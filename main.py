cc4e302970a20477bca578f89272a27178f08f93
cristian.torres.ds <cristian.torres.ds@gmail.com>
2023-07-31 22:21:51 -0300

    probando

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI()
-
+# Título y descripción de la API
+app = FastAPI(title='PROYECTO INDIVIDUAL Nº1 - Machine Learning Operations (MLOps) - Cristian Gabriel Torres DataFT13',
+              description='API de datos y predicción de precios de videojuegos')
 
 @app.get("/inicio")
 async def ruta_prueba():
